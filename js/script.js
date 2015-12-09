7d34770403e15edc9bdfcdbe5e0697d52886320d
mortendk <morten@geekroyale.com>
2015-12-09 02:12:25 +0100

    menu corrected with fonts added + dummy picts

--- js/script.js
+++ js/script.js
@@ -1,5 +1,32 @@
-// vanilla.js
-// js drupal documentation
 (function ($) {
+
+  // adds in a fixed position menu
+  // css in page.css navigation fixed
+  $(window).scroll(function() {
+    if( $(this).scrollTop() > $('.hero').height() ) {
+      $('.hero-nav').addClass('hero-nav--locked');
+    } else {
+      $('.hero-nav').removeClass('hero-nav--locked');
+    }
+  });
+  // smooth scroll
+  $(function() {
+    // console.log( $('.hero-nav').height());
+
+    $('a[href*=#]:not([href=#])').click(function() {
+      if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+        if (target.length) {
+          $('html,body').animate({
+            // calculates hero-nav height
+            scrollTop: target.offset().top - $('.hero-nav').height()
+          }, 1000);
+          return false;
+        }
+      }
+    });
+  });
 
 })(jQuery);
